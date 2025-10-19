621c6c1820939caa6a41ce6ace9f9fb13c1ed4d0
Imran Ali <imran30599@gmail.com>
2025-10-19 19:39:56 +0530

    add file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './camponents/Card.jsx'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid veritatis error et voluptatibus corporis eos dolore a. Veniam ex quis necessitatibus. Accusantium, voluptates aliquid. Nemo quas facere quaerat dolores laboriosam.</h1>
+      <Card></Card>
     </>
   )
 }
