f646c69c1d603bbdf0523df4737020dbacb12bab
Imran Ali <imran30599@gmail.com>
2025-10-20 22:09:01 +0530

    Modified and add file

--- src/camponents/Hero.jsx
+++ src/camponents/Hero.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Hero = () => {
   return (
     <main className='hero'>
-        <h1>Welcome to the Hero Component</h1>
-        <p>This is the hero section of the webpage. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum tenetur numquam est, omnis blanditiis excepturi deserunt saepe illo doloremque eum accusantium at! Quisquam, blanditiis adipisci autem quis illum ad necessitatibus.</p>
+        <div>Div 1</div>
+        <div>Div 2</div>
     </main>
   )
 }
