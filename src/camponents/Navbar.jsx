6245ecbb02763140bf7b26f99b396acd3a4b9f72
Imran Ali <imran30599@gmail.com>
2025-10-20 19:27:39 +0530

    Modified

---
+++ src/camponents/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className="navbar">
+        <div></div>
+        <div></div>
+    </header>
+  )
+}
+
+export default Navbar
