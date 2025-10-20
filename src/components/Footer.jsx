a59974c50821cb01f97c68904e396c98b642c25c
Imran Ali <imran30599@gmail.com>
2025-10-20 23:11:40 +0530

    Modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>
-        Footer Component
-    </div>
-  )
+    return (
+        <footer className="footer">
+            <div>Footer Div 1</div>
+            <div>Footer Div 2</div>
+        </footer>
+    )
 }
 
 export default Footer
