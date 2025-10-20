830ee33d43cd190c74056e7609de8aa904afe84c
Imran Ali <imran30599@gmail.com>
2025-10-20 21:45:56 +0530

    Modified

--- src/camponents/Navbar.jsx
+++ src/camponents/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <header className="navbar">
-        <div>Logo</div>
-        <div>Anchor</div>
-    </header>
-  )
+    return (
+        <header className="navbar">
+            <div>Logo</div>
+            <div>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Skills</a>
+                <a href="#">Project</a>
+                <a href="#">Contect</a>
+            </div>
+        </header>
+    )
 }
 
 export default Navbar
