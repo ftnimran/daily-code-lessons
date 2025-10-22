27d16e3c5cb03f5811c45a3a948ed96bfc1c366e
Imran Ali <imran30599@gmail.com>
2025-10-22 21:00:59 +0530

    Modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <header className="navbar">
-            <div>Logo</div>
-            <div>
+        <header className="header">
+            <a href="#" className='logo'>Logo</a>
+
+            <nav className="navbar">
                 <a href="#">Home</a>
                 <a href="#">About</a>
                 <a href="#">Skills</a>
                 <a href="#">Project</a>
                 <a href="#">Contect</a>
-            </div>
+            </nav>
         </header>
     )
 }
