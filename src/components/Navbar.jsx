8fc6023919223cd04186a6992d8f5c7e0752c259
Imran Ali <imran30599@gmail.com>
2025-10-24 21:46:56 +0530

    modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import logoIcon from '../assets/image/icon_01.png'
 // import "./Alibhai.css"; // optional for styling
 
 const Navbar = () => {
@@ -10,7 +11,7 @@ const Navbar = () => {
   return (
     <div>
       <header>
-        <img src="image/icon_01.png" className="logo" alt="Logo" />
+        <img src={logoIcon} className="logo" alt="Logo" />
         <button className="menu-btn" onClick={openSidebar}>
           ☰
         </button>
@@ -19,12 +20,12 @@ const Navbar = () => {
           <button className="close-btn" onClick={closeSidebar}>
             ✖
           </button>
-          <a href="#home" onClick={closeSidebar}>Home</a>
-          <a href="#about" onClick={closeSidebar}>About</a>
-          <a href="#skills" onClick={closeSidebar}>Skills</a>
-          <a href="#projects" onClick={closeSidebar}>Projects</a>
-          <a href="#contact" onClick={closeSidebar}>Contact</a>
-          <a href="#resume" onClick={closeSidebar}>Resume</a>
+          <a href="#home" style={{ "--i": 1 }} onClick={closeSidebar}>Home</a>
+          <a href="#about" style={{ "--i": 2 }} onClick={closeSidebar}>About</a>
+          <a href="#skills" style={{ "--i": 3 }} onClick={closeSidebar}>Skills</a>
+          <a href="#projects" style={{ "--i": 4 }} onClick={closeSidebar}>Projects</a>
+          <a href="#contact" style={{ "--i": 5 }} onClick={closeSidebar}>Contact</a>
+          <a href="#resume" style={{ "--i": 6 }} onClick={closeSidebar}>Resume</a>
         </nav>
       </header>
 
