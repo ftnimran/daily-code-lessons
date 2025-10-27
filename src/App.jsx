f31bac26b1738d84a2d59bee30378dbe9b34a04c
Imran Ali <imran30599@gmail.com>
2025-10-27 22:48:27 +0530

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
-// import Sections from './components/Sections.jsx'
+import Sections from './components/Sections.jsx'
 import Footer from './components/Footer.jsx'
 
 
@@ -14,7 +14,7 @@ function App() {
     <>  
         <Navbar />
         <Hero />
-        {/* <Sections /> */}
+        <Sections />
         <Footer />
     </>
   )
--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import backGround2 from '../assets/image/background_image02.png'
 
 const Section = () => {
   return (
-    <section id='sections' className='sections'>
-        <div>Section Div 1</div>
-        <div>Section Div 2</div>
+    <section id="about" class="about">
+      <div class="about-img">
+        <img src={backGround2} />
+      </div>
+      <div class="about-text">
+        <h2>About <span>Me</span></h2>
+        <h4>Full Stack Developer!</h4>
+        <p>I am a skilled web designer with over 3 years of experience in the industry. My passion lies in creating
+          captivating website designs and implementing them through frontend development. Itake pride in staying
+          up-to-date with current design trends and leveraging my creativity to produce visually appealing and
+          user-friendly websites. Throughout my career, I have developed a deep understanding of user experience
+          (UX) and user interface (UI) principles. By putting myself in the shoes of the end-users, I strive to
+          create intuitive and seamless browsing experiences. I believe that a well-designed website should not
+          only look visually appealing but also provide a smooth and enjoyable interaction for visitors. When
+          starting a new project, I thoroughly research and analyze the target audience and the client's specific
+          requirements. This enables me to tailor my designs to meet their expectations and deliver a unique
+          online presence that aligns with their brand identity..</p>
+        <a href="#" class="btn-box">More About Me</a>
+      </div>
     </section>
   )
 }
