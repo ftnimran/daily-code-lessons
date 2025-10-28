cda46825ba0bb83a19422502744b041b66747a28
Imran Ali <imran30599@gmail.com>
2025-10-29 01:29:48 +0530

    Modified

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -10,7 +10,7 @@ const Section = () => {
       </div>
 
       <div className="about-container">
-        
+
         <div class="about-img">
           <img src={backGround2} />
         </div>
@@ -23,9 +23,9 @@ const Section = () => {
             up-to-date with current design trends and leveraging my creativity to produce visually appealing and
             user-friendly websites. Throughout my career, I have developed a deep understanding of user experience
             (UX) and user interface (UI) principles. By putting myself in the shoes of the end-users, I strive to
-            {/* create intuitive and seamless browsing experiences. I believe that a well-designed website should not
+            create intuitive and seamless browsing experiences. I believe that a well-designed website should not
             only look visually appealing but also provide a smooth and enjoyable interaction for visitors. When
-            starting a new project, I thoroughly research and analyze the target audience and the client's specific */}
+            starting a new project, I thoroughly research and analyze the target audience and the client's specific
             requirements. This enables me to tailor my designs to meet their expectations and deliver a unique
             online presence that aligns with their brand identity..</p>
           <a href="#" class="btn-box">More About Me</a>
