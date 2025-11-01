94d3981258e42f841be9a728d318031a9cde9ead
Imran Ali <imran30599@gmail.com>
2025-11-01 23:40:39 +0530

    Modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,8 +10,8 @@ const About = () => {
       </div>
 
       <div className='about-container'>
-        <div>Div 1</div>
-        <div>Div 2</div>
+        <div className='about-picture'>Div 1     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic commodi veritatis esse maiores, facilis non voluptate minus id repellendus blanditiis aperiam optio labore ipsam iste quis aspernatur atque, natus eaque?</div>
+        <div className='about-content'>Div 2    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, in obcaecati. Fugit repellat asperiores cum, nisi debitis unde! Ratione reiciendis dolorum fuga voluptatibus culpa iusto ut vitae, dignissimos accusantium porro.</div>
       </div>
 
     </section>
