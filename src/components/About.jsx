b515c26de9c26d3c5299f73f6b5682110256ac57
Imran Ali <imran30599@gmail.com>
2025-11-02 22:13:26 +0530

    Modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,11 +16,14 @@ const About = () => {
         <div className='about-content'>
           <h1>I'm Imran Ali</h1>
           <h5>MERN Stack Developer</h5>
-          <p>I’m a passionate MERN Stack Developer who loves building modern, responsive, and high-performance web applications.
+          <p>I'm a passionate MERN Stack Developer who loves building modern, responsive, and high-performance web applications.
             I work with MongoDB, Express.js, React, and Node.js to create seamless full-stack solutions that deliver great user experiences.
             My focus is on writing clean, scalable, and efficient code with attention to design and functionality.
             I enjoy solving complex problems, learning new technologies, and improving performance with every project.
             With a balance of creativity and logic, I aim to develop digital products that make a real impact.</p>
+          <a href="#" className="btn-box">
+            More About Me
+          </a>
         </div>
       </div>
 
